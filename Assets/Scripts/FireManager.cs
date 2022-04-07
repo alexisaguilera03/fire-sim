faaff2f74b48f527706d9d7e0d6c6df3f38f0ef5
alexisaguilera03@gmail.com <alexisaguilera03@gmail.com>
2022-04-07 16:35:25 -0500

    save work

--- Assets/Scripts/FireManager.cs
+++ Assets/Scripts/FireManager.cs
@@ -46,6 +46,7 @@ public class FireManager : MonoBehaviour
         tmp.RemoveAll(x => x.gameObject.transform.root.CompareTag("Player"));
         tmp.RemoveAll(x => x.gameObject.transform.root.CompareTag("Fire"));
         tmp.RemoveAll(x => x.gameObject.transform.root.CompareTag("Extinguisher"));
+        tmp.RemoveAll(x => x.gameObject.transform.root.CompareTag("InteractionSystem"));
         hitColliders = tmp.ToArray();
         GameObject newObject = hitColliders[rng.Next(0, hitColliders.Length)].gameObject;
         createFire(newObject.transform.position, FireGameObject.transform.rotation);
